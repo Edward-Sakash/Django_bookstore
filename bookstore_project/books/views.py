60f82a0e5519b9207f24ddacc8d08eaf9398ee5f
Edward-Sakash <edward.sakash@yahoo.com>
2023-10-25 14:23:43 +0200

    unfinished yet

--- bookstore_project/books/views.py
+++ bookstore_project/books/views.py
@@ -4,23 +4,32 @@ from .models import Book
 
 class BookListView(ListView):
     model = Book
-    template_name = 'books/book_list.html'
+    template_name = 'book_list.html'
+    context_object_name = 'book_list'
+    
+    def get(self, request, *args, **kwargs):
+        books_queryset = self.get_queryset()
+        books_list = list(books_queryset)
+        print(books_list)
+
+        return super().get(request, *args, **kwargs)
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'books/book_detail.html'
+    template_name = 'book_detail.html'
 
 class BookUpdateView(UpdateView):
     model = Book
-    template_name = 'books/book_form.html'
+    template_name = 'book_form.html'
     fields = ['title', 'author', 'description', 'published_date', 'price']
 
 class BookCreateView(CreateView):
     model = Book
-    template_name = 'books/book_form.html'
+    template_name = 'book_form.html'
     fields = ['title', 'author', 'description', 'published_date', 'price']
 
 class BookDeleteView(DeleteView):
     model = Book
-    template_name = 'books/book_confirm_delete.html'
+    template_name = 'book_confirm_delete.html'
     success_url = reverse_lazy('books:book_list')
+
